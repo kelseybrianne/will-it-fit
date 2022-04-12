741382207eb8188bc7d4a8e667cebc02df63fb2b
Kim (she/hers) windows <kimberlym4488@comcast.net>
2022-04-11 17:31:57 -0700

    Starting auth.js, server.js, and jwt code

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -10,6 +11,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
