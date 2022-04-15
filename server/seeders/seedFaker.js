96527251b3a29da745ce00dea1cb94954b73b61d
Kim (she/hers) windows <kimberlym4488@comcast.net>
2022-04-14 22:22:20 -0700

    seedFaker updated, change loop numbers for different seed #'s

--- server/seeders/seedFaker.js
+++ server/seeders/seedFaker.js
@@ -37,57 +37,84 @@ db.once('open', async () => {
   try {
     await User.deleteMany({});
     await Item.deleteMany({});
-    let itemData = [];
-    for (let i = 0; i < 10; i++) {
-      let items = {
-        category: randomCategory(),
-        style: faker.commerce.productAdjective(),
-        brand: faker.commerce.productMaterial(),
-        name: faker.commerce.productName(),
-        gender: gender(),
-        size: faker.mersenne.rand(0, 20),
-        link: faker.internet.url(),
-        photo: faker.image.fashion(),
-        color: faker.internet.color(),
-        review: faker.commerce.productDescription(),
-      };
-      itemData.push(items);
-    }
-    await Item.insertMany(itemData);
-    console.log('Items seeded! :)');
+    // let itemData = [];
+    // for (let i = 0; i < 3; i++) {
+    //   let items = {
+    //     category: randomCategory(),
+    //     style: faker.commerce.productAdjective(),
+    //     brand: faker.commerce.productMaterial(),
+    //     name: faker.commerce.productName(),
+    //     gender: gender(),
+    //     size: faker.mersenne.rand(0, 20),
+    //     link: faker.internet.url(),
+    //     photo: faker.image.fashion(),
+    //     color: faker.internet.color(),
+    //     review: faker.commerce.productDescription(),
+    //   };
+    //   itemData.push(items);
+    // }
+    // await Item.insertMany(itemData);
+    // console.log('Items seeded! :)');
 
-    // get list of newly created itemId's to pass through to user data.
-    let itemID = [];
-    const itemSearch = await Item.find({}).distinct('_id', {});
-    itemID.push(itemSearch);
-    console.log(itemID);
-
-    // call this function to return random item ID.
-    function itemIndex() {
-      const index = Math.floor(Math.random() * 10);
-      return itemID[index];
-    }
-    console.log(itemIndex());
-
-    // make a bunch of user data
+    // }
     let userData = [];
+    // console.log(itemIndex());
     for (let i = 0; i < 5; i++) {
-      const firstName = faker.name.firstName();
-      const lastName = faker.name.lastName();
+      // make a bunch of user data
 
-      let newDay = {
-        username: [firstName, lastName].join(''),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-        weight: randomIntFromInterval(100, 400),
-        height: randomIntFromInterval(55, 85),
-        primaryPhoto: faker.image.avatar(),
-        closet: itemIndex(),
-        savedItems: itemIndex(),
-      };
-      userData.push(newDay);
+      for (let i = 0; i < 2; i++) {
+        const firstName = faker.name.firstName();
+        const lastName = faker.name.lastName();
+        const name = [firstName, lastName].join('');
+
+        let itemData = [];
+        for (let i = 0; i < 5; i++) {
+          let items = {
+            category: randomCategory(),
+            style: faker.commerce.productAdjective(),
+            brand: faker.commerce.productMaterial(),
+            name: faker.commerce.productName(),
+            gender: gender(),
+            size: faker.mersenne.rand(0, 20),
+            link: faker.internet.url(),
+            photo: `https://loremflickr.com/640/480/outfit?random=${Math.round(
+              Math.random() * 1000
+            )}`,
+            color: faker.internet.color(),
+            review: faker.commerce.productDescription(),
+          };
+          itemData.push(items);
+        }
+        await Item.create(itemData);
+
+        // // get list of newly created itemId's to pass through to user data.
+        // const itemID = Item.find({}).distinct('_id', {});
+        let itemID = [];
+        const itemSearch = await Item.find({}).populate('_id', {});
+        itemID.push(itemSearch);
+        console.log(itemID);
+
+        // call this function to return random item ID.
+        function itemIndex() {
+          const index = Math.floor(Math.random() * 4);
+          return itemID[index];
+        }
+
+        let newDay = {
+          username: name,
+          email: faker.internet.email(),
+          password: faker.internet.password(),
+          weight: randomIntFromInterval(100, 400),
+          height: randomIntFromInterval(55, 85),
+          primaryPhoto: faker.image.avatar(),
+          closet: itemIndex(),
+          savedItems: itemIndex(),
+          followers: [],
+          following: [],
+        };
+        userData.push(newDay);
+      }
     }
-
     // // For each of the users that exist, add 2 items.
     // tags.forEach(() => makePost(getRandomPost(50)));
 
