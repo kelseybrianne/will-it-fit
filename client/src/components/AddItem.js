34f955bc83e09d4e2bba78261bd7659247d6c29b
Mikayla Bruce <mkla.w.b@gmail.com>
2022-04-16 14:08:59 -0700

    styling changes.

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -8,7 +8,6 @@ import MenuItem from '@mui/material/MenuItem';
 
 import FormControl from '@mui/material/FormControl';
 import { Dialog } from '@mui/material';
-// import Menu from '@mui/material/Menu';
 
 // graphQL:
 import { useMutation } from '@apollo/client';
@@ -22,8 +21,21 @@ const genderDB = [
   { label: 'Mens', value: 'Mens' },
   { label: 'Unisex', value: 'Unisex' },
 ];
+const colorDB = [
+  { label: 'Red', value: 'Red' },
+  { label: 'Orange', value: 'Orange' },
+  { label: 'Yellow', value: 'Yellow' },
+  { label: 'Green', value: 'Green' },
+  { label: 'Blue', value: 'Blue' },
+  { label: 'Purple', value: 'Purple' },
+  { label: 'Pink', value: 'Pink' },
+  { label: 'White', value: 'White' },
+  { label: 'Grey', value: 'Grey' },
+  { label: 'Black', value: 'Black' },
+  
+];
 const AddItem = () => {
-  const [open, setOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false);
   const [userFormData, setUserFromData] = React.useState({
     category: '',
     size: '',
@@ -78,154 +90,165 @@ const AddItem = () => {
 
   return (
     <div>
-    <Button onClick={(e)=> {
-      setOpen(true)
-      e.preventDefault()
-      }} variant="contained">Add Item</Button> 
-    
-    <Dialog open={open} onClose={() => setOpen(false)} id="form">
-      <Box
-        // validate={validated}
-        onSubmit={formSubmit}
-        component="form"
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '25ch' },
-        }}
-      >
-        {/* category */}
-        <FormControl>
-          <TextField
-            name="category"
-            label="* Category"
-            variant="filled"
-            onChange={handleChange}
-            value={userFormData.category}
-          />
-        </FormControl>
-
-        {/* size */}
-        <FormControl>
-          <TextField
-            name="size"
-            label="* Size"
-            variant="filled"
-            onChange={handleChange}
-            value={userFormData.size}
-          />
-        </FormControl>
-
-        {/* photo */}
-        <FormControl>
-          <TextField
-            name="photo"
-            label="* Photo"
-            variant="filled"
-            onChange={handleChange}
-            value={userFormData.photo}
-          />
-        </FormControl>
-
-        {/* style */}
-        <FormControl>
-          <TextField
-            name="style"
-            label="Style"
-            variant="filled"
-            onChange={handleChange}
-            value={userFormData.style}
-          />
-        </FormControl>
-
-        {/* brand */}
-        <FormControl>
-          <TextField
-            name="brand"
-            label="Brand"
-            variant="filled"
-            onChange={handleChange}
-            value={userFormData.brand}
-          />
-        </FormControl>
-
-        {/* name */}
-        <FormControl>
-          <TextField
-            name="name"
-            label="* Name"
-            variant="filled"
-            onChange={handleChange}
-            value={userFormData.name}
-          />
-        </FormControl>
-
-        {/* gender - drop down menu */}
-        <FormControl>
-          <TextField
-            name="gender"
-            select
-            value={userFormData.gender}
-            label="Gender"
-            variant="filled"
-            onChange={handleChange}
-          >
-            {genderDB.map((option) => (
-              <MenuItem size="large" key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-        </FormControl>
-
-        {/* link */}
-        <FormControl>
-          <TextField
-            name="link"
-            label="Link"
-            variant="filled"
-            onChange={handleChange}
-            value={userFormData.link}
-          />
-        </FormControl>
-
-        {/* color */}
-        <FormControl>
-          <TextField
-            name="color"
-            label="Color"
-            variant="filled"
-            onChange={handleChange}
-            value={userFormData.color}
-          />
-        </FormControl>
-
-        {/* review */}
-        <TextareaAutosize
-          name="review"
-          variant="filled"
-          aria-label="minimum height"
-          minRows={5}
-          placeholder="Add your review here"
-          style={{ width: 435 }}
-          onChange={handleChange}
-          value={userFormData.review}
-        />
-      
-
       <Button
-        // disabled={
-        //   !(
-        //     userFormData.category &&
-        //     userFormData.name &&
-        //     userFormData.size &&
-        //     userFormData.photo
-        //   )}
-        type="submit"
+        onClick={(e) => {
+          setOpen(true);
+          e.preventDefault();
+        }}
         variant="contained"
       >
-        Submit Item
+        Add Item
       </Button>
-      </Box>
-    </Dialog>
+
+      <Dialog open={open} onClose={() => setOpen(false)} id="form">
+        <Box
+          // validate={validated}
+          onSubmit={formSubmit}
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+        >
+          {/* category */}
+          <FormControl>
+            <TextField
+              name="category"
+              label="* Category"
+              variant="filled"
+              onChange={handleChange}
+              value={userFormData.category}
+            />
+          </FormControl>
+
+          {/* size */}
+          <FormControl>
+            <TextField
+              name="size"
+              label="* Size"
+              variant="filled"
+              onChange={handleChange}
+              value={userFormData.size}
+            />
+          </FormControl>
+
+          {/* photo */}
+          <FormControl>
+            <TextField
+              name="photo"
+              label="* Photo"
+              variant="filled"
+              onChange={handleChange}
+              value={userFormData.photo}
+            />
+          </FormControl>
+
+          {/* style */}
+          <FormControl>
+            <TextField
+              name="style"
+              label="Style"
+              variant="filled"
+              onChange={handleChange}
+              value={userFormData.style}
+            />
+          </FormControl>
+
+          {/* brand */}
+          <FormControl>
+            <TextField
+              name="brand"
+              label="Brand"
+              variant="filled"
+              onChange={handleChange}
+              value={userFormData.brand}
+            />
+          </FormControl>
+
+          {/* name */}
+          <FormControl>
+            <TextField
+              name="name"
+              label="* Name"
+              variant="filled"
+              onChange={handleChange}
+              value={userFormData.name}
+            />
+          </FormControl>
+
+          {/* gender - drop down menu */}
+          <FormControl>
+            <TextField
+              name="gender"
+              select
+              value={userFormData.gender}
+              label="Gender"
+              variant="filled"
+              onChange={handleChange}
+            >
+              {genderDB.map((option) => (
+                <MenuItem size="large" key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </FormControl>
+
+          {/* link */}
+          <FormControl>
+            <TextField
+              name="link"
+              label="Link"
+              variant="filled"
+              onChange={handleChange}
+              value={userFormData.link}
+            />
+          </FormControl>
+
+          {/* color */}
+          <FormControl>
+            <TextField
+              name="color"
+              select
+              value={userFormData.color}
+              label="Color"
+              variant="filled"
+              onChange={handleChange}
+            >
+              {colorDB.map((option) => (
+                <MenuItem size="large" key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </FormControl>
+
+          {/* review */}
+          <TextareaAutosize
+            name="review"
+            variant="filled"
+            aria-label="minimum height"
+            minRows={5}
+            placeholder="Add your review here"
+            style={{ width: 435 }}
+            onChange={handleChange}
+            value={userFormData.review}
+          />
+
+          <Button
+            // disabled={
+            //   !(
+            //     userFormData.category &&
+            //     userFormData.name &&
+            //     userFormData.size &&
+            //     userFormData.photo
+            //   )}
+            type="submit"
+            variant="contained"
+          >
+            Submit Item
+          </Button>
+        </Box>
+      </Dialog>
     </div>
   );
 };
