3e6b1320002a4457ff00374d8adb418d9a86d02c
Kim (she/hers) windows <kimberlym4488@comcast.net>
2022-04-18 13:07:27 -0700

    merge with main

--- client/src/components/LogIn/SignUpForm.js
+++ client/src/components/LogIn/SignUpForm.js
@@ -1,13 +1,33 @@
 import { useMutation } from '@apollo/client';
 import { Button, Container, TextField, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import auth from '../../utils/auth';
-import { ADD_USER } from '../../utils/mutations';
+import { ADD_USER, SINGLE_UPLOAD } from '../../utils/mutations';
 
 /** This is a dialog form for signing up for an account */
 function SignUpForm({ setDialogState }) {
   const [signup, { error, data }] = useMutation(ADD_USER);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  useEffect(() => {
+    if (selectedImage) {
+      setImageUrl(URL.createObjectURL(selectedImage));
+    }
+  }, [selectedImage]);
+  const [uploadFile] = useMutation(SINGLE_UPLOAD, {
+    onCompleted: (data) => console.log(data),
+  });
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+    console.log(file);
+    uploadFile({ variables: { file } });
+  };
+
   const [formState, setFormState] = useState({
     username: '',
     email: '',
@@ -80,6 +100,26 @@ function SignUpForm({ setDialogState }) {
         >
           Will It Fit?
         </Typography>
+        {/* image upload */}
+        <input
+        accept="image/*"
+        type="file"
+        id="select-image"
+        style={{ display: 'none' }}
+        onChange={(e) => setSelectedImage(e.target.files[0])}
+      />
+      <label htmlFor="select-image">
+        <Button variant="contained" color="primary" component="span">
+          Upload Image
+        </Button>
+      </label>
+        {imageUrl && selectedImage && (
+        <Box mt={2} textAlign="center">
+          <div>Image Preview:</div>
+          <img src={imageUrl} alt={selectedImage.name} height="100px" />
+        </Box>
+         )}
+          { /* Begin standard form questions */}
         <TextField
           autoFocus
           margin="dense"
@@ -135,6 +175,7 @@ function SignUpForm({ setDialogState }) {
           value={formState.shoeSize}
           sx={{ backgroundColor: 'white' }}
         />
+        
         <Button
           type="submit"
           variant="contained"
