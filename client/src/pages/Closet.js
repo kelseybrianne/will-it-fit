727cc4c014f0ec9f8bd41b45bdb253dc4c2c9c8b
Kelsey Mailau <kelseymailau@gmail.com>
2022-04-20 15:22:55 -0700

    show discover carousel in profile on toggle and add discover button next to add item

--- client/src/pages/Closet.js
+++ client/src/pages/Closet.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useParams } from 'react-router-dom';
 // eslint-disable-next-line no-unused-vars
 import Button from '@mui/material/Button';
@@ -11,6 +11,7 @@ import Stack from '@mui/material/Stack';
 import profilePic from '../assets/images/atikh-bana-_KaMTEmJnxY-unsplash.jpg';
 import AddItem from '../components/AddItem';
 import DiscoverCarousel from '../components/DiscoverCarousel';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 
 import { useQuery } from '@apollo/client';
 import { GET_USER } from '../utils/queries';
@@ -21,20 +22,26 @@ const Closet = ({ windowSize }) => {
   // eslint-disable-next-line no-unused-vars
   const { loading, data } = useQuery(GET_USER, {
     variables: {
-      username: username
+      username: username,
     },
   });
 
+  const discoverCarousel = document.querySelector('.toggle-discover-carousel');
+  const toggleDiscoverCarousel = () => {
+    discoverCarousel.style.display === 'none'
+      ? (discoverCarousel.style.display = 'block')
+      : (discoverCarousel.style.display = 'none');
+  };
+
   const userData = data?.user || {};
-  console.log(userData);
 
   return (
     <div className="profile-page">
       <div className="white-div"></div>
       <div className="profile-head">
         <div className="profile-img-div">
-          <img src={userData.primaryPhoto} alt={userData.primaryPhoto}/>
-          <h2>{userData.username}</h2>
+          <img src={userData.primaryPhoto} alt={userData.primaryPhoto} />
+          {/* <h2>{userData.username}</h2> */}
           <div className="folls-div">
             <a href="tbd">
               <p>Following</p>
@@ -46,10 +53,24 @@ const Closet = ({ windowSize }) => {
           </div>
           {/* toggle 'Following' and 'Follow' on click'*/}
           {/* <button className="unfollow">Following</button> */}
-          <AddItem />
+        </div>
+        <div className="username-div">
+          <h2 className="closet-username">{userData.username}</h2>
+          <div className="btns-div">
+            <AddItem />
+            <Button
+              variant="contained"
+              className="discover-btn"
+              onClick={toggleDiscoverCarousel}
+            >
+              <PersonAddAltIcon />
+            </Button>
+          </div>
         </div>
       </div>
-      <DiscoverCarousel />
+      <div className="toggle-discover-carousel">
+        <DiscoverCarousel />
+      </div>
       <Items userData={userData} windowSize={windowSize} />
     </div>
   );
