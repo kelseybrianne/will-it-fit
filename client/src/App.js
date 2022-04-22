fe6493d6bee6938c15f8b41e2b739155e61da7ca
Kelsey Mailau <kelseymailau@gmail.com>
2022-04-22 16:33:16 -0700

    add footer content with GitHub repo url and pin footer to bottom of page

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import { setContext } from '@apollo/client/link/context';
 import Search from './pages/Search';
 import SavedItems from './pages/SavedItems/index.js';
 import Home from './pages/Home/index.js';
+import Footer from './components/Footer/Footer.js';
 
 // Construct our main GraphQL API endpoint
 // const httpLink = createHttpLink({
@@ -47,7 +48,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <>
+        <div className="body-container">
           <Header />
           <Routes>
             <Route path="/discover" element={<DiscoverFeed />} />
@@ -56,7 +57,10 @@ function App() {
             <Route path="/search" element={<Search />} />
             <Route path="/saved" element={<SavedItems />} />
           </Routes>
-        </>
+        </div>
+        <div className="footer-container">
+          <Footer />
+        </div>
       </Router>
     </ApolloProvider>
   );
