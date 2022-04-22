1b36a90a952ba4352625f9f801b8dec74b09b64e
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-04-21 19:35:09 -0700

    Connected the ItemList to the data returned from the GET_FAVORITES query

--- client/src/pages/SavedItems/index.js
+++ client/src/pages/SavedItems/index.js
@@ -1,14 +1,33 @@
 import { useQuery } from '@apollo/client';
+import { Container, Typography } from '@mui/material';
+import ItemList from '../../components/ItemList';
 import auth from '../../utils/auth';
 import { GET_FAVORITES } from '../../utils/queries';
 
 export default function SavedItems() {
   const me = auth.getProfile();
-  console.log('me.data._id', me.data._id);
   const { data, error, loading } = useQuery(GET_FAVORITES, {
     variables: { id: me.data._id },
   });
-  console.log(data);
 
-  return <>Saved Items</>;
+  if (error) {
+    return <Typography>{error.message}</Typography>;
+  }
+
+  if (loading) {
+    return <Typography>Loading...</Typography>;
+  }
+
+  return (
+    <Container>
+      <h1
+        style={{
+          padding: '2rem 0',
+        }}
+      >
+        Saved Items
+      </h1>
+      <ItemList items={data?.savedItems}></ItemList>
+    </Container>
+  );
 }
