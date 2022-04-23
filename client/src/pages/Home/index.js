c33a85a0f3bdf515248860c0544d69563210dfa7
Mikayla Bruce <mkla.w.b@gmail.com>
2022-04-22 20:56:58 -0700

    so close to getting this to work

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,9 +2,9 @@ import { useQuery } from '@apollo/client';
 import { Container, Typography } from '@mui/material';
 import ItemList from '../../components/ItemList';
 import { GET_FEED } from '../../utils/queries';
-import DiscoverFeed from '../../components/DiscoverCarousel/DiscoverCarousel'
-
-import auth from '../../utils/auth'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import auth from '../../utils/auth';
 export default function Home() {
   const { data, error, loading } = useQuery(GET_FEED);
 
@@ -17,20 +17,37 @@ export default function Home() {
     return <Typography>Loading...</Typography>;
   }
 
-  {auth.loggedIn() ? <Home /> : <DiscoverFeed />}
+  if (auth.loggedIn)
+    return (
+      <Container>
+        <h1
+          style={{
+            padding: '2rem 0',
+            testAlign: 'center',
+          }}
+        >
+          The latest and greatest from people you follow:
+        </h1>
+        <ItemList items={data?.feed}></ItemList>
+      </Container>
+    );
 
-  return (
-    <Container>
-      <h1
-        style={{
-          padding: '2rem 0',
-          testAlign: 'center',
-        }}
-      >
-        The latest and greatest from people you follow:
-      </h1>
-      <ItemList items={data?.feed}></ItemList>
-    
-    </Container>
-  );
+  if (!auth.loggedIn)
+    return (
+      <div className="carousel-div">
+        <div className="inner-carousel-div">
+          <div className="carousel-container" id="responsive-container">
+            <div className="carousel-inner"></div>
+          </div>
+          <div className="nav">
+            <button className="prev">
+              {/* <ChevronLeftIcon onClick={} /> */}
+            </button>
+            <button className="next">
+              {/* <ChevronRightIcon onClick={nextPage} /> */}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
 }
