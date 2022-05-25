c2c45783b1bcf2a869efc23e309ce1d4a806064d
Kelsey Mailau <kelseymailau@gmail.com>
2022-05-24 20:52:22 -0700

    update headings to be more clear and add heading to contributors page

--- client/src/pages/Contributors/index.js
+++ client/src/pages/Contributors/index.js
@@ -5,67 +5,68 @@ import kimProfile from '../../assets/images/Kimothy.jpg';
 
 const index = () => {
   return (
-    <div className={styles.container}>
-      <a
-        className={styles.contributor}
-        href="https://github.com/kelseybrianne"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <div>
-          <img
-            className={styles.profile}
-            src={kelseyProfile}
-            alt="kelsey's profile"
-          />
-          <p>Kelsey Betteridge</p>
-        </div>
-      </a>
-      <a
-        className={styles.contributor}
-        href="https://github.com/kimberlym4488"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <div>
-          <img
-            className={styles.profile}
-            src={kimProfile}
-            alt="kim's profile"
-          />
-          <p>Kimberly Moran</p>
-        </div>
-      </a>
-      <a
-        className={styles.contributor}
-        href="https://github.com/jmichaelbrown8"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <div>
-          <img
-            className={styles.profile}
-            src="{}"
-            alt="J's profile"
-          />
-          <p>J. Michael Brown</p>
-        </div>
-      </a>
-      <a
-        className={styles.contributor}
-        href="https://github.com/MikiWolfe"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <div>
-          <img
-            className={styles.profile}
-            src="{kelseyProfile}"
-            alt="mikayla's profile"
-          />
-          <p>Mikayla Bruce</p>
-        </div>
-      </a>
+    <div>
+      <h2 className="page-header">
+        Contributors
+      </h2>
+      <div className={styles.container}>
+        <a
+          className={styles.contributor}
+          href="https://github.com/kelseybrianne"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div>
+            <img
+              className={styles.profile}
+              src={kelseyProfile}
+              alt="kelsey's profile"
+            />
+            <p>Kelsey Betteridge</p>
+          </div>
+        </a>
+        <a
+          className={styles.contributor}
+          href="https://github.com/kimberlym4488"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div>
+            <img
+              className={styles.profile}
+              src={kimProfile}
+              alt="kim's profile"
+            />
+            <p>Kimberly Moran</p>
+          </div>
+        </a>
+        <a
+          className={styles.contributor}
+          href="https://github.com/jmichaelbrown8"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div>
+            <img className={styles.profile} src="{}" alt="J's profile" />
+            <p>J. Michael Brown</p>
+          </div>
+        </a>
+        <a
+          className={styles.contributor}
+          href="https://github.com/MikiWolfe"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div>
+            <img
+              className={styles.profile}
+              src="{kelseyProfile}"
+              alt="mikayla's profile"
+            />
+            <p>Mikayla Bruce</p>
+          </div>
+        </a>
+      </div>
     </div>
   );
 };
